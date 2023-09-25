40d66961bec6c7909341582a26364b5daabcc4f9
Nick Spencer <strangebrew202@gmail.com>
2023-09-25 09:21:14 -0600

    Add: associations to pass initial relationships spec tests

--- spec/models/scientist_spec.rb
+++ spec/models/scientist_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Scientist do
   it {should belong_to :lab}
   it {should have_many :experiment_scientists}
-  it {should have_many(:experiements).through(:experiment_scientists)}
+  it {should have_many(:experiments).through(:experiment_scientists)}
 end
