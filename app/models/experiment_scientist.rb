939ea17cad4254385737923aa869cfd40d1f5491
Nick Spencer <strangebrew202@gmail.com>
2023-09-25 11:30:22 -0600

    Formating

--- app/models/experiment_scientist.rb
+++ app/models/experiment_scientist.rb
@@ -1,6 +1,7 @@
 class ExperimentScientist < ApplicationRecord
   belongs_to :scientist
   belongs_to :experiment
+  
   def self.assigned_exp_scientist(scientist_id, experiment_id)
     where("scientist_id = #{scientist_id} and experiment_id = #{experiment_id}")
     .first
