72150823623d106ef29eec42649ef270c88c2257
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-25 10:15:04 -0600

    test: add test for long_running_descending experiment model method

--- spec/models/experiment_spec.rb
+++ spec/models/experiment_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe Experiment, type: :model do
     it { should have_many :scientist_experiments }
     it { should have_many(:scientists).through(:scientist_experiments) }
   end
+
+  describe ".long_running_descending" do
+    it "should return only the experiments running longer than 6 months in descending order" do
+      @experiment_1 = Experiment.create(
+        name: "MINERvA",
+        objective: "study neutrino reactions with five different nuclei",
+        num_months: 9
+      )
+  
+      @experiment_2 = Experiment.create(
+        name: "Cell Structure under Intense Heat",
+        objective: "study the changes in cell structure in extreme heat",
+        num_months: 5
+      )
+  
+      @experiment_3 = Experiment.create(
+        name: "How silly is too silly",
+        objective: "understanding the impact of being silly",
+        num_months: 15
+      )
+
+      expect(Experiment.long_running_descending).to eq([@experiment_3, @experiment_1])
+    end
+  end
 end
