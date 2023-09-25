e4bc32de2355cf2c488e2eedcaa954418733822c
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-09-25 10:41:05 -0500

    testing, views, routes and controllers for user story 1

---
+++ spec/features/scientists/show_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "the scientist show page" do
+  it "shows scientists information, the name of the lab, and all experiments they are working on" do
+    lab_1 = Lab.create(name: "Fermilab")
+    scientist_1 = Scientist.create(name: "Marie Curie", specialty: "radioactivity", university: "University of Paris", lab: lab_1)
+    experiment_1 = Experiment.create(name: "MINERvA", objective: "study neutrino reactions with five different nuclei", num_months: 4)
+    experiment_2 = Experiment.create(name: "Radioactive-TV", objective: "is the TV radioactive?", num_months: 7)
+    experiment_3 = Experiment.create(name: "Tree-Therapy", objective: "does the apple fall far from the tree?", num_months: 4)
+    sci_exp_1 = ScientistExperiment.create(experiment: experiment_1, scientist: scientist_1)
+    sci_exp_2 = ScientistExperiment.create(experiment: experiment_2, scientist: scientist_1)
+
+    visit scientist_path(scientist_1)
+
+    within(".scientist_info") do
+      expect(page).to have_content("Scientist: #{scientist_1.name}")
+      expect(page).to have_content("Specialty: #{scientist_1.specialty}")
+      expect(page).to have_content("University: #{scientist_1.university}")
+      expect(page).to have_content("Current Lab: #{scientist_1.lab.name}")
+    end
+    within(".experiments") do
+      expect(page).to have_content(experiment_1.name)
+      expect(page).to have_content(experiment_2.name)
+      expect(page).to_not have_content(experiment_3.name)
+    end
+  end
+end
+
