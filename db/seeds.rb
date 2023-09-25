6eac22f7d0a9acab38df574cfd06605f8e45f110
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-25 09:35:47 -0600

    feat: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+@lab_1 = Lab.create(name: "Fermilab")
+
+@scientist_1 = @lab_1.scientists.create(
+  name: "Marie Curie",
+  specialty: "radioactivity",
+  university: "University of Paris"
+)
+
+@experiment_1 = @scientist_1.experiments.create(
+  name: "MINERvA",
+  objective: "study neutrino reactions with five different nuclei",
+  num_months: 4
+)
+
+@experiment_2 = @scientist_1.experiments.create(
+  name: "Cell Structure under Intense Heat",
+  objective: "study the changes in cell structure in extreme heat",
+  num_months: 2
+)
