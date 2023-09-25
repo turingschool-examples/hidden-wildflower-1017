0dbfa4408392215c7040922164c00ea5ae5963e0
Antoine Aube <aaube3@gmail.com>
2023-09-25 10:43:53 -0600

    query: add query for sorting by number of months greater than 6

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,4 +1,8 @@
 class Experiment < ApplicationRecord
   has_many :scientist_experiments
   has_many :scientists, through: :scientist_experiments
+  
+  def self.sort_by_num_months_longer_than_six
+    Experiment.order(num_months: :desc).where("num_months > 6")
+  end
 end
