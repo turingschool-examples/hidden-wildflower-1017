e8e04efe8f2e8173b9da3da94f3dbfadef634c70
Will Weston <Williamweston81@gmail.com>
2023-09-25 12:47:19 -0500

    Weston(3) Test: add description in instances Test: add description in expectations Add: description in experiments index view

--- spec/features/experiments/index_spec.rb
+++ spec/features/experiments/index_spec.rb
@@ -3,20 +3,25 @@ require "rails_helper"
 RSpec.describe "Experiments Index Page" do
   describe "As a visitor" do
     it "I see the names of all experiments whose length is greater than 6 months and they are ordered from longest to shortest" do
-      experiment_1 = Experiment.create!(name: "Actinium", num_months: 12)
-      experiment_2 = Experiment.create!(name: "Niobium", num_months: 11)
-      experiment_3 = Experiment.create!(name: "Promethium", num_months: 8)
-      experiment_4 = Experiment.create!(name: "Argon", num_months: 7)
-      experiment_5 = Experiment.create!(name: "Radon", num_months: 4)
-      experiment_6 = Experiment.create!(name: "Silicon", num_months: 3)
+      experiment_1 = Experiment.create!(name: "Actinium", description: "Porro iste architecto eum.", num_months: 12)
+      experiment_2 = Experiment.create!(name: "Niobium", description: "Expedita error dolor corrupti.", num_months: 11)
+      experiment_3 = Experiment.create!(name: "Promethium", description: "Quis ut aut odit.", num_months: 8)
+      experiment_4 = Experiment.create!(name: "Argon", description: "Eos qui deleniti possimus.", num_months: 7)
+      experiment_5 = Experiment.create!(name: "Radon", description: "Quas dolores fuga reiciendis.", num_months: 4)
+      experiment_6 = Experiment.create!(name: "Silicon", description: "Ipsa dolorem doloremque dolorem.", num_months: 3)
 
       visit "/experiments"
 
       within "#Experiments" do
         expect(page).to have_content(experiment_3.name)
+        expect(page).to have_content(experiment_3.description)
+        expect(page).to have_content(experiment_3.num_months)
+
         expect(page).to have_content(experiment_1.name)
+
         expect(page).to_not have_content(experiment_5.name)
         expect(page).to_not have_content(experiment_6.name)
+        
         expect(experiment_1.name).to appear_before(experiment_3.name)
         expect(experiment_3.name).to appear_before(experiment_4.name)
       end
--- spec/models/experiment_spec.rb
+++ spec/models/experiment_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Experiment do
   describe "class methods" do
     describe "::experiment_length" do
       it "returns experiments with length greater than 6 months" do
-        experiment_1 = Experiment.create!(name: "Actinium", num_months: 12)
-        experiment_2 = Experiment.create!(name: "Niobium", num_months: 11)
-        experiment_3 = Experiment.create!(name: "Promethium", num_months: 8)
-        experiment_4 = Experiment.create!(name: "Argon", num_months: 7)
-        experiment_5 = Experiment.create!(name: "Radon", num_months: 4)
-        experiment_6 = Experiment.create!(name: "Silicon", num_months: 3)
+        experiment_1 = Experiment.create!(name: "Actinium", description: "Porro iste architecto eum.", num_months: 12)
+        experiment_2 = Experiment.create!(name: "Niobium", description: "Expedita error dolor corrupti.", num_months: 11)
+        experiment_3 = Experiment.create!(name: "Promethium", description: "Quis ut aut odit.", num_months: 8)
+        experiment_4 = Experiment.create!(name: "Argon", description: "Eos qui deleniti possimus.", num_months: 7)
+        experiment_5 = Experiment.create!(name: "Radon", description: "Quas dolores fuga reiciendis.", num_months: 4)
+        experiment_6 = Experiment.create!(name: "Silicon", description: "Ipsa dolorem doloremque dolorem.", num_months: 3)
 
         expect(Experiment.experiment_length).to eq([experiment_1, experiment_2, experiment_3, experiment_4])
       end
