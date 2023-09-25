3db806b94af56fd7a3eaaba4506dda727d401c40
Lane Bretschneider <lanebret85@gmail.com>
2023-09-25 08:16:12 -0700

    test: add/update tests for relationships

--- spec/models/lab_spec.rb
+++ spec/models/lab_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Lab do
-  it {should have_many :scientists}
+RSpec.describe Lab, type: :model do
+  describe "relationships" do
+    it { should have_many :scientists }
+  end
 end
