6dec86f03453e1f46b58d3ac089a7623b215ea3c
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-09-25 11:00:28 -0600

    add tests for extension

---
+++ spec/features/labs/show_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "Labs Show", type: :feature do
+  before(:each) do
+    @lab_1 = Lab.create!(name: "The Testing Locale")
+    @lab_1 = Lab.create!(name: "The Fake Lab")
+    @scientist_1 = Scientist.create!(name: "Chicken", specialty: "Being afraid", university: "Kiwi's Bird School", lab_id: @lab_1.id)
+    @scientist_2 = Scientist.create!(name: "Coco", specialty: "Low brain power", university: "Kiwi's Bird School", lab_id: @lab_1.id)
+    @scientist_3 = Scientist.create!(name: "Hiccup", specialty: "Poor flight control", university: "Kiwi's Bird School", lab_id: @lab_1.id)
+    @scientist_4 = Scientist.create!(name: "Kila", specialty: "Not even a bird", university: "None", lab_id: @lab_2.id)
+    @experiment_1 = Experiment.create!(name: "BrainP", objective: "Determine if we have brains at all", num_months: 17)
+    @experiment_2 = Experiment.create!(name: "FlAP", objective: "(Fl)y (A)ll over the (P)lace", num_months: 3)
+    @experiment_3 = Experiment.create!(name: "EATM", objective: "EAT ALL THE MILLET", num_months: 50)
+    ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_2.id)
+    ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_3.id)
+    ScientistExperiment.create!(scientist_id: @scientist_2.id, experiment_id: @experiment_1.id)
+    ScientistExperiment.create!(scientist_id: @scientist_2.id, experiment_id: @experiment_2.id)
+    ScientistExperiment.create!(scientist_id: @scientist_2.id, experiment_id: @experiment_3.id)
+    ScientistExperiment.create!(scientist_id: @scientist_3.id, experiment_id: @experiment_3.id)
+  end
+
+  describe "When I visit the labs show page" do
+    it "I see the labs name, and the names of all scientists that work at the lab" do
+      visit "/labs/#{@lab_1}"
+
+      expect(page).to have_content(@lab_1.name)
+      expect(page).to have_content(@scientist_1.name)
+      expect(page).to have_content(@scientist_2.name)
+      expect(page).to have_content(@scientist_3.name)
+      expect(page).to_not have_content(@scientist_4.name)
+    end
+
+    it "I see the number of experiments associated with each scientist next to their name, and they're sorted by number of experiments descending" do
+      visit "/labs/#{@lab_1}"
+
+      expect(page).to have_content("#{@scientist_1.name} - Experiments: 2")
+      expect(page).to have_content("#{@scientist_2.name} - Experiments: 3")
+      expect(page).to have_content("#{@scientist_3.name} - Experiments: 1")
+
+      expect(@scientist_2.name).to appear_before(@scientist_1.name, only_text: true)
+      expect(@scientist_1.name).to appear_before(@scientist_3.name, only_text: true)
+    end
+  end
+end
--- spec/models/lab_spec.rb
+++ spec/models/lab_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Lab do
   it {should have_many :scientists}
+
+  describe "instance methods" do
+    before(:each) do
+      @lab_1 = Lab.create!(name: "The Testing Locale")
+      @lab_1 = Lab.create!(name: "The Fake Lab")
+      @scientist_1 = Scientist.create!(name: "Chicken", specialty: "Being afraid", university: "Kiwi's Bird School", lab_id: @lab_1.id)
+      @scientist_2 = Scientist.create!(name: "Coco", specialty: "Low brain power", university: "Kiwi's Bird School", lab_id: @lab_1.id)
+      @scientist_3 = Scientist.create!(name: "Hiccup", specialty: "Poor flight control", university: "Kiwi's Bird School", lab_id: @lab_1.id)
+      @scientist_4 = Scientist.create!(name: "Kila", specialty: "Not even a bird", university: "None", lab_id: @lab_2.id)
+      @experiment_1 = Experiment.create!(name: "BrainP", objective: "Determine if we have brains at all", num_months: 17)
+      @experiment_2 = Experiment.create!(name: "FlAP", objective: "(Fl)y (A)ll over the (P)lace", num_months: 3)
+      @experiment_3 = Experiment.create!(name: "EATM", objective: "EAT ALL THE MILLET", num_months: 50)
+      ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_2.id)
+      ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_3.id)
+      ScientistExperiment.create!(scientist_id: @scientist_2.id, experiment_id: @experiment_1.id)
+      ScientistExperiment.create!(scientist_id: @scientist_2.id, experiment_id: @experiment_2.id)
+      ScientistExperiment.create!(scientist_id: @scientist_2.id, experiment_id: @experiment_3.id)
+      ScientistExperiment.create!(scientist_id: @scientist_3.id, experiment_id: @experiment_3.id)
+    end
+
+    describe "#scientists_by_experiment_count" do
+      it "returns a list of scientists for a given lab, sorted by number of experiments they're associated with" do
+        expect(@lab_1.scientists_by_experiment_count).to eq([@scientist_2, @scientist_1, @scientist_3])
+      end
+    end
+  end
 end
