5a2b8e7f4f3b54843ed035fa7634d3c77380094c
Lane Bretschneider <lanebret85@gmail.com>
2023-09-25 10:53:39 -0700

    test: add model test for scientist_experiment_counts method

--- spec/models/scientist_spec.rb
+++ spec/models/scientist_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Scientist, type: :model do
     it { should have_many :scientist_experiments }
     it { should have_many(:experiments).through(:scientist_experiments) }
   end
+
+  describe "scientist_experiment_counts" do
+    it "returns the number of experiments that each scientist is working on and orders the results from most number of experiments to least number of experiments" do
+      query = Scientist.scientist_experiment_counts
+
+      expect(query.map { |scientist| scientist.attributes }).to eq([{id: nil, name: "Albert Einstein", experiment_count: 3}, {id: nil, name: "Marie Curie", experiment_count: 2}])
+    end
+  end
 end
