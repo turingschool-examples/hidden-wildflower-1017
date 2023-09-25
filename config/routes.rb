762cdfbea826542694a755d654d283b76aab038f
Lane Bretschneider <lanebret85@gmail.com>
2023-09-25 09:58:58 -0700

    feat: add route for delete a scientist_experiment

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :scientists, only: [:show]
+  
+  delete "/scientists/:scientist_id/experiments/:experiment_id", to: "scientist_experiments#destroy"
 end
