8bdc6c225296defff5fc8cf25767f0deaad13406
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-25 10:59:25 -0600

    feat: update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 @lab_1 = Lab.create(name: "Fermilab")
 
 @scientist_1 = @lab_1.scientists.create(
@@ -23,13 +15,13 @@
 @experiment_1 = Experiment.create(
   name: "MINERvA",
   objective: "study neutrino reactions with five different nuclei",
-  num_months: 9
+  num_months: 4
 )
 
 @experiment_2 = Experiment.create(
   name: "Cell Structure under Intense Heat",
   objective: "study the changes in cell structure in extreme heat",
-  num_months: 5
+  num_months: 2
 )
 
 @experiment_3 = Experiment.create(
@@ -41,3 +33,5 @@
 ScientistExperiment.create(scientist: @scientist_1, experiment: @experiment_1)
 ScientistExperiment.create(scientist: @scientist_1, experiment: @experiment_2)
 ScientistExperiment.create(scientist: @scientist_2, experiment: @experiment_1)
+ScientistExperiment.create(scientist: @scientist_2, experiment: @experiment_2)
+ScientistExperiment.create(scientist: @scientist_2, experiment: @experiment_3)
