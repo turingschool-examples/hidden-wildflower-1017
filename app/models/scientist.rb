d26574cb6826db7a560e201faa011c6022439c57
Will Weston <Williamweston81@gmail.com>
2023-09-25 11:19:27 -0500

    Weston(6) Add: experiments to Scientist_spec Test: scientist show spec - pass Add: Routes for scientists show Create: Scientist show html Add: associations for scientist Create: scientists controller and show action

--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -1,4 +1,5 @@
 class Scientist < ApplicationRecord
   belongs_to :lab
-  has_many :experiments
+  has_many :scientists_experiments
+  has_many :experiments, through: :scientists_experiments
 end
---
+++ spec/features/scientists/show_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Scientists Show Page" do
+  describe "As a visitor" do
+    it "I see the scientist name, specialty, university including the scientist's lab and experiments" do
+      lab_1 = Lab.create!(name: "Kling LLC")
+      scientist_1 = lab_1.scientists.create!(name: "Waylon Runolfsdottir", specialty: "Physics", university: " Deckow Institute")
+      experiment_1 = scientist_1.experiments.create!(name: "Argon")
+      experiment_2 = scientist_1.experiments.create!(name: "Niobium")
+
+
+      visit "/scientists/#{scientist_1.id}"
+
+      within("#Scientist-#{scientist_1.id}") do
+
+      expect(page).to have_content(scientist_1.name)
+      expect(page).to have_content(scientist_1.specialty)
+      expect(page).to have_content(scientist_1.university)
+      end
+
+      within("#Scientist_Lab-#{lab_1.id}") do
+      expect(page).to have_content(lab_1.name)
+
+      end
+
+      within("#Scientist_Exp-#{scientist_1.id}") do
+      expect(page).to have_content(experiment_1.name)
+      expect(page).to have_content(experiment_2.name)
+      end
+    end
+  end
+end
