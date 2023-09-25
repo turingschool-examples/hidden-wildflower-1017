28cb1600de26afd9f73a466600d428333120a820
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-09-25 10:08:34 -0600

    add tests for US2

--- spec/features/scientists/show_spec.rb
+++ spec/features/scientists/show_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe "Scientists Show", type: :feature do
   before(:each) do
     @lab_1 = Lab.create!(name: "The Testing Locale")
     @scientist_1 = Scientist.create!(name: "Chicken", specialty: "Being afraid", university: "Kiwi's Bird School", lab_id: @lab_1.id)
+    @scientist_2 = Scientist.create!(name: "Coco", specialty: "Low brain power", university: "Kiwi's Bird School", lab_id: @lab_1.id)
     @experiment_1 = Experiment.create!(name: "BrainP", objective: "Determine if we have brains at all", num_months: 17)
     @experiment_2 = Experiment.create!(name: "FlAP", objective: "(Fl)y (A)ll over the (P)lace", num_months: 3)
     @experiment_3 = Experiment.create!(name: "EATM", objective: "EAT ALL THE MILLET", num_months: 50)
     ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_1.id)
     ScientistExperiment.create!(scientist_id: @scientist_1.id, experiment_id: @experiment_3.id)
+    ScientistExperiment.create!(scientist_id: @scientist_2.id, experiment_id: @experiment_2.id)
+    ScientistExperiment.create!(scientist_id: @scientist_2.id, experiment_id: @experiment_3.id)
   end
 
   describe "When I visit the scientists show page" do
@@ -31,6 +34,35 @@ RSpec.describe "Scientists Show", type: :feature do
 
       expect(page).to_not have_content(@experiment_2.name)
       save_and_open_page
+    end
+
+    it "I see a button next to each experiment that removes that experiment from the scientists workload" do
+      visit "/scientists/#{@scientist_1.id}"
+
+      within("#experiment-#{@experiment_1.id}") do
+        expect(page).to have_button("Remove Experiment")
+      end
+
+      within("#experiment-#{@experiment_3.id}") do
+        expect(page).to have_button("Remove Experiment")
+        click_button("Remove Experiment")
+      end
+
+      expect(page).to have_current_path("/scientists/#{@scientist_1.id}")
+      expect(page).to have_content(@experiment_1.name)
+      expect(page).to_not have_content(@experiment_3.name)
+    end
+
+    it "A removed experiment does not impact other scientists working on that experiment" do
+      visit "/scientists/#{@scientist_1.id}"
+
+      within("#experiment-#{@experiment_3.id}") do
+        click_button("Remove Experiment")
+      end
+
+      visit "/scientists/#{@scientist_2.id}"
+
+      expect(page).to have_content(@experiment_3.name)
     end
   end
 end
