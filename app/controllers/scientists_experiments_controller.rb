4ae25b35d72243ce844329b4a35ad42bba5587ed
Will Weston <Williamweston81@gmail.com>
2023-09-25 12:53:58 -0500

    update delete to destroy for standardized formatting

--- app/controllers/scientists_experiments_controller.rb
+++ app/controllers/scientists_experiments_controller.rb
@@ -4,8 +4,9 @@ class ScientistsExperimentsController < ApplicationController
     @scientist = Scientist.find(params[:scientist_id])
     @experiment = Experiment.find(params[:id])
 
-    @scientist.experiments.delete(@experiment)
+    @scientists_experoments = ScientistsExperiment.find_by(scientist_id: @scientist.id, experiment_id: @experiment.id)
 
+    @scientists_experoments.destroy
     redirect_to scientist_path(@scientist)
   end
 end
