7793bf7efdf18a05800a92ae8646b354bd2e7686
Lane Bretschneider <lanebret85@gmail.com>
2023-09-25 10:54:54 -0700

    feat: add scientist_experiment_counts model method

--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -2,4 +2,8 @@ class Scientist < ApplicationRecord
   belongs_to :lab
   has_many :scientist_experiments
   has_many :experiments, through: :scientist_experiments
+
+  def self.scientist_experiment_counts
+    Scientist.joins(:experiments).select("scientists.name, count(scientist_experiments) as experiment_count").group("scientists.name").order("experiment_count DESC")
+  end
 end
