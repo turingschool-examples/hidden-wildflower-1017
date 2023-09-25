fce43a47e2c91933c7fc984970664a3c444c4cc5
Will Chen <wfchen1990@gmail.com>
2023-09-25 09:21:16 -0600

    Add experiment model and associations

--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -1,3 +1,5 @@
 class Scientist < ApplicationRecord
   belongs_to :lab
+  has many :scientist_experiments
+  has many :experiments, through: :scientist_experiments
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# lab1 = Lab.create!(name: "Los Alamos National Laboratory")
+
+# scientist1 = lab1.scientists.create!(name: "Robert Oppenheimer", specialty: "Physics", university: "University of Chicago")
+
+# experiment1 = Experiment.create!(name: "Manhattan Project", objective: "Develop the first nuclear weapons", num_months: 36)
+
+# scientist1.experiments << experiment1
