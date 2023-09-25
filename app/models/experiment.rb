ed19eea327142c9137778e65a166cc1dabf3b70c
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-09-25 10:39:06 -0600

    add query to experiment model for US3

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,4 +1,10 @@
 class Experiment < ApplicationRecord
   has_many :scientist_experiments
   has_many :scientists, through: :scientist_experiments
+
+  def self.long_experiments
+    self
+      .where("num_months >= 6")
+      .order("num_months DESC")
+  end
 end
