b645078971e4d871af59c056c5f47a29c032a9ec
Will Chen <wfchen1990@gmail.com>
2023-09-25 09:56:04 -0600

    Adjust US2 route

--- app/controllers/scientist_experiments_controller.rb
+++ app/controllers/scientist_experiments_controller.rb
@@ -1,6 +1,6 @@
 class ScientistExperimentsController < ApplicationController
   def destroy
-    scientist_experiment = ScientistExperiment.find_by(scientist_id: params[:scientist_id], experiment_id: params[:id])
+    scientist_experiment = ScientistExperiment.find_by(scientist_id: params[:scientist_id], experiment_id: params[:experiment_id])
     scientist_experiment.destroy
     redirect_to show_scientist_path(params[:scientist_id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/scientists/:scientist_id', to: 'scientists#show', as: 'show_scientist'
-  delete '/scientists/:scientist_id/experiments/:id', to: 'scientist_experiments#destroy', as: 'destroy_scientist_experiment'
+
+  delete '/scientists/:scientist_id/experiments/:experiment_id', to: 'scientist_experiments#destroy', as: 'destroy_scientist_experiment'
+
+  get '/experiments', to: 'experiments#index', as: 'experiments'
 end
