0c344cdf28ded529ef4dbfb791d9815b317dfa43
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-25 10:59:15 -0600

    feat: add scientists_descending lab model method

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,3 +1,7 @@
 class Lab < ApplicationRecord
   has_many :scientists
+  
+  def scientists_descending
+    scientists.joins(:experiments).select("scientists.*, experiments.count as experiment_count").group("scientists.id").order("experiment_count desc")
+  end
 end
